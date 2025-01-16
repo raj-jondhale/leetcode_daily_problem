db011c3ddea087bc1445b2bccd500ccffc1def0d
Rajesh Jondhale <128236746+raj-jondhale@users.noreply.github.com>
2025-01-16 23:26:58 +0530

    Time: 0 ms (100%), Space: 64.2 MB (64.63%) - LeetHub

--- 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.cpp
+++ 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.cpp
@@ -4,24 +4,18 @@ public:
         int m=nums1.size();
         int n=nums2.size();
 
-        unordered_map<int,long>mp;
+        int XOR=0;
 
-        for(auto &num :nums1){
-            mp[num]+=n;
-        }
-
-        for(auto &num:nums2){
-            mp[num]+=m;
-        }
-        int result=0;
-        for(auto &it:mp){
-            int num=it.first;
-            int freq=it.second;
-
-            if(freq%2 !=0){
-                result^=num;
+        if(m%2 != 0){
+            for(auto &num:nums2){
+                XOR ^=num;
             }
-        } 
-        return result;       
+        }
+        if(n%2 !=0){
+            for(auto &num:nums1){
+                XOR ^=num;
+            }
+        }
+        return XOR;
     }
 };
