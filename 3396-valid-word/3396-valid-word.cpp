72e9dac6200e56fab4ae823eb05d548642995428
Rajesh Jondhale <128236746+raj-jondhale@users.noreply.github.com>
2025-07-15 10:38:19 +0530

    Time: 0 ms (100%), Space: 8 MB (67.48%) - LeetHub

---
+++ 3396-valid-word/3396-valid-word.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string word) {
+        int n=word.size();
+
+        if(n<3) return false;
+        
+
+        bool hasVowel=false;
+        bool hasConsonant=false;
+
+        for(char ch:word){
+            if(isalpha(ch)){
+                ch=tolower(ch);
+                if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+                    hasVowel=true;
+                }else{
+                    hasConsonant=true;
+                }
+            }else if(!isdigit(ch)){
+                    return false;
+            }
+        }
+        return hasVowel && hasConsonant;
+    }
+};
