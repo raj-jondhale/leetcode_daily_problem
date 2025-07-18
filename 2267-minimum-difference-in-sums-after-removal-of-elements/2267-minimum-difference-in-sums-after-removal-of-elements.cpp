78e5e284d4e5c382c45b5b2f1abf1de592197c4d
Rajesh Jondhale <128236746+raj-jondhale@users.noreply.github.com>
2025-07-18 20:05:02 +0530

    Time: 80 ms (96.65%), Space: 154.8 MB (54.12%) - LeetHub

---
+++ 2267-minimum-difference-in-sums-after-removal-of-elements/2267-minimum-difference-in-sums-after-removal-of-elements.cpp
@@ -0,0 +1,64 @@
+#define ll long long
+class Solution {
+public:
+    ll minimumDifference(vector<int>& nums) {
+        int n = nums.size();
+        int k = n / 3;
+
+        // Step 1: Build prefix sums using max-heap
+        priority_queue<int> maxHeap;
+        vector<ll> prefix(n, 0);
+        ll sum = 0;
+
+        // process starting k element
+        for(int i = 0; i < k; ++i){
+            maxHeap.push(nums[i]);
+            sum += nums[i];
+        }
+        prefix[k-1] = sum;
+        
+        // process more element and choose k only
+        for(int i = k; i < 2 * k; ++i){
+            if(maxHeap.top() > nums[i]){
+                prefix[i] = prefix[i-1] - maxHeap.top() + nums[i];
+
+                maxHeap.pop();
+                maxHeap.push(nums[i]);
+            }
+            else {
+                prefix[i] = prefix[i-1]; // carry forward the previous sum
+            }
+        }
+
+        // Step 2: Build suffix sums using min-heap
+        priority_queue<int, vector<int>, greater<>> minHeap;
+        vector<ll> suffix(n, 0);
+        sum = 0;
+
+        for(int i = n - 1; i >= n - k; --i){
+            minHeap.push(nums[i]);
+            sum += nums[i];
+        }
+        suffix[n - k] = sum;
+
+        for(int i = n - k - 1; i >= k; --i){
+            if(minHeap.top() < nums[i]){
+                suffix[i] = suffix[i+1] - minHeap.top() + nums[i];
+
+                minHeap.push(nums[i]);
+                minHeap.pop();
+            } 
+            else {
+                suffix[i] = suffix[i+1]; // carry forward the previous sum
+            }
+        }
+
+        // Step 3: Compute minimum difference of partitions
+        ll ans = LLONG_MAX;
+        for(int i = k - 1; i < 2 * k; ++i){
+            ans = min(ans, prefix[i] - suffix[i + 1]);
+        }
+
+        return ans;
+    }
+};
