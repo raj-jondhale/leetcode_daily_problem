0939d12c311b8f081a2f2d3c9a6074245452ec40
Rajesh Jondhale <128236746+raj-jondhale@users.noreply.github.com>
2025-08-16 09:01:57 +0530

    Time: 0 ms (100%), Space: 8.1 MB (12.18%) - LeetHub

--- 1448-maximum-69-number/1448-maximum-69-number.cpp
+++ 1448-maximum-69-number/1448-maximum-69-number.cpp
@@ -1,19 +1,14 @@
+// C++
 class Solution {
 public:
     int maximum69Number (int num) {
-        string str=to_string(num);
-
-        int n=str.length();
-        int res=num;
-        for(int i=0;i<n;i++){
-            if(str[i]=='6'){
-                str[i]='9';
-                if(stoi(str)>res){
-                    res=stoi(str);
-                    str[i]='6';
-                }
+        string s = to_string(num);
+        for (char &c : s) {
+            if (c == '6') { 
+                c = '9'; 
+                break; 
             }
         }
-        return res;
+        return stoi(s);
     }
 };
