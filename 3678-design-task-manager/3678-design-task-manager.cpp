3660ebe791d1587aa4546dacb85cd206ba91779f
Rajesh Jondhale <128236746+raj-jondhale@users.noreply.github.com>
2025-09-18 21:45:04 +0530

    Time: 237 ms (80.94%), Space: 366.5 MB (66.88%) - LeetHub

---
+++ 3678-design-task-manager/3678-design-task-manager.cpp
@@ -0,0 +1,37 @@
+class TaskManager {
+    typedef pair<int, int> Task; 
+    priority_queue<Task> maxHeap;                  
+    unordered_map<int, int> taskPriorityMap;       
+    unordered_map<int, int> taskOwnerMap;          
+
+public:
+    TaskManager(vector<vector<int>>& initialTasks) {
+        for (const auto& task : initialTasks) {
+            add(task[0], task[1], task[2]);
+        }
+    }
+    void add(int userId, int taskId, int priority) {
+        maxHeap.push({priority, taskId});
+        taskPriorityMap[taskId] = priority;
+        taskOwnerMap[taskId] = userId;
+    }
+    void edit(int taskId, int newPriority) {
+        maxHeap.push({newPriority, taskId});
+        taskPriorityMap[taskId] = newPriority;
+    }  
+    void rmv(int taskId) {
+        taskPriorityMap[taskId] = -1; 
+    }
+    int execTop() {
+        while (!maxHeap.empty()) {
+            auto [priority, taskId] = maxHeap.top();
+            maxHeap.pop();
+
+            if (priority == taskPriorityMap[taskId]) {
+                taskPriorityMap[taskId] = -1; 
+                return taskOwnerMap[taskId];
+            }
+        }
+        return -1; 
+    }
+};
