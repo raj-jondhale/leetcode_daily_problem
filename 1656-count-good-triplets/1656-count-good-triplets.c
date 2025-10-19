8593ff577ba1f8e3d43abb9fcebc4bb5b1238fdd
Rajesh Jondhale <128236746+raj-jondhale@users.noreply.github.com>
2025-10-19 15:56:17 +0530

    Time: 8 ms (77.59%), Space: 8.4 MB (2.59%) - LeetHub

--- 1656-count-good-triplets/1656-count-good-triplets.c
+++ 1656-count-good-triplets/1656-count-good-triplets.c
@@ -1,15 +1,21 @@
+int absDiff(int x, int y) {
+    return x > y ? x - y : y - x;
+}
 
-int countGoodTriplets(int* arr, int arrSize, int a, int b, int c) {
-    int x = 0, i, j, k;
-    for (i = 0; i < arrSize - 2;i++) { 
-        for (j = i + 1; j < arrSize - 1;j++) {  
-            for (k = j + 1; k < arrSize;k++) { 
-                if ((abs(arr[i] - arr[j])) <= a &&
-                    (abs(arr[j] - arr[k])) <= b &&
-                    (abs(arr[i] - arr[k])) <=c) 
-                    x++;
+int countGoodTriplets(int* arr, int arrSize, int a, int b, int c){
+    int count = 0;
+
+    for (int i = 0; i < arrSize - 2; i++) {
+        for (int j = i + 1; j < arrSize - 1; j++) {
+            if (absDiff(arr[i], arr[j]) <= a) { // first condition
+                for (int k = j + 1; k < arrSize; k++) {
+                    if (absDiff(arr[j], arr[k]) <= b && absDiff(arr[i], arr[k]) <= c) {
+                        count++;
+                    }
+                }
             }
         }
     }
-    return x;
+
+    return count;
 }
