b8132f84d863999a4d327cc7ca298bb3df802c7b
Rajesh Jondhale <128236746+raj-jondhale@users.noreply.github.com>
2025-12-24 10:29:59 +0530

    Time: 0 ms (100%), Space: 33 MB (92.95%) - LeetHub

---
+++ 3074-apple-redistribution-into-boxes/3074-apple-redistribution-into-boxes.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minimumBoxes(vector<int>& apple, vector<int>& capacity) {
+        sort(begin(capacity), end(capacity), greater<int>()); 
+
+        int totalApple = accumulate(begin(apple), end(apple), 0);
+
+        int count = 0;
+        int i = 0;
+        while(totalApple > 0) {
+            totalApple -= capacity[i];
+            count++;
+            i++;
+        }
+
+        return count;         
+    }
+};
