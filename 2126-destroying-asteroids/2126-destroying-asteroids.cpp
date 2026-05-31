ad3430c4321d3111b3418d4ed0bdf12aff2200ee
Rajesh Jondhale <128236746+raj-jondhale@users.noreply.github.com>
2026-05-31 23:26:58 +0530

    Time: 30 ms (69.63%), Space: 106.9 MB (16.2%) - LeetHub

---
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        sort(asteroids.begin(),asteroids.end());
+
+        for(int m:asteroids ){
+            if(mass<m){
+                return false;
+            }else if (mass>=1e5){
+                return true;
+            }
+            mass+=m;
+        }
+        return true;
+    }
+};
